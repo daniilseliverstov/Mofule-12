d9c85d7c8b5646e351f490898dc48169a212e37f
daniilseliverstov <selivrstov91@internet.ru>
2024-09-03 07:04:37 +0300

    Методы Юнит-тестирования

---
+++ test/test_2.py
@@ -0,0 +1,47 @@
+import unittest
+import runner_and_tournament as rat
+
+
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runner1 = rat.Runner('Усэйн', 10)
+        self.runner2 = rat.Runner('Андрей', 9)
+        self.runner3 = rat.Runner('Ник', 5)
+
+    @classmethod
+    def tearDownClass(cls):
+        res_dict = {}
+        for kay, value in cls.all_results.items():
+            for k, v in value.items():
+                res_dict[k] = str(v)
+            print(res_dict)
+
+    def test_1(self):
+        first_run = rat.Tournament(90, self.runner1, self.runner3)
+        result = first_run.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+    def test_2(self):
+        second_run = rat.Tournament(90, self.runner2, self.runner3)
+        result = second_run.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+    def test_3(self):
+        third_run = rat.Tournament(90, self.runner1, self.runner2, self.runner3)
+        result = third_run.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+
+if __name__ == '__main__':
+    unittest.main()
